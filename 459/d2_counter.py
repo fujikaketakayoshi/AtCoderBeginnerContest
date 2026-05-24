7d60b90ea95b96080df0074770d440bd031e8d49
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-05-24 17:16:30 +0900

    もっと不要変数とか綺麗にした版

--- 459/d2_counter.py
+++ 459/d2_counter.py
@@ -8,21 +8,13 @@ N = int(input())
 for _ in range(N):
   S = input().strip()
   cnt = Counter(S)
-  words = list(cnt.keys())
-  # most = cnt.most_common(1)
-  # cnt = sorted(cnt.items(), key=lambda x: x[1], reverse=True)
   max_cnt = cnt.most_common(1)[0][1]
-  # max_c = most[0][0]
-  all_cnt = 0
-  for v in cnt.values():
-    all_cnt += v
-  if max_cnt - (all_cnt - max_cnt) <= 1:
+  if max_cnt - (len(S) - max_cnt) <= 1:
     print('Yes')
     
     hq = []  # 空のヒープを作る
     for k, v in cnt.items():
       heapq.heappush(hq, [-v, k])
-    # print(hq)
     max_v, max_k = heapq.heappop(hq)
     results = [max_k]
     pre = [max_v + 1, max_k]
