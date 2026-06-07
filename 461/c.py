7bea9149d89a61196cfada4d8ef0ecf6fa87d780
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-06-07 16:25:27 +0900

    最初にM個の宝石を各種類の最大値から取ってきて、その取った宝石のインデックスを記録しておいて、残りをそのインデックス以外から降順で取得していきK個に達したところで答え

---
+++ 461/c.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+N, K, M = map(int, input().split())
+# print(N, K, M)
+
+VC = []
+# Cs = [[] for _ in range(N + 1)]
+Cmax = defaultdict(int)
+Cidx = defaultdict(int)
+for n in range(N):
+  C, V = map(int, input().split())
+  if Cmax[C] < V:
+    Cmax[C] = V
+    Cidx[C] = n
+  VC.append((V, C, n))
+VC.sort(reverse=True)
+
+sorted_Cmax = sorted(Cmax.items(), key=lambda x: x[1], reverse=True)
+# print(sorted_Cmax)
+
+used_idx = set()
+ans = 0
+cnt = 0
+for i in range(M):
+  C = sorted_Cmax[i][0]
+  V = sorted_Cmax[i][1]
+  used_idx.add(Cidx[C])
+  ans += V
+  cnt += 1
+
+for V, C, n in VC:
+  if K <= cnt:
+    break
+  if n in used_idx:
+    continue
+  ans += V
+  cnt += 1
+
+print(ans)
