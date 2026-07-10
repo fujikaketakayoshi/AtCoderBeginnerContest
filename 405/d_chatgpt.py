a59192d95c10fc11378886266835910b6afb0b25
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-07-10 14:50:59 +0900

    座席からではなくEから多始点BFSを行うことでEからの最短距離マスを辿ることができる。Eから上下左右への移動に対して、v^><をマスに記録してけば答えとなる

---
+++ 405/d_chatgpt.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+H, W = map(int, input().split())
+# print(H, W)
+grid = []
+Es = []
+ans = []
+visited = [[False] * W for _ in range(H)]
+for h in range(H):
+  row = list(input().strip())
+  grid.append(row)
+  ans.append(row[:])
+  for w, r in enumerate(row):
+    if r == 'E':
+      Es.append((h, w))
+      visited[h][w] = True
+# print(grid)
+# print(Es)
+# print(visited)
+
+DIRS = [(-1, 0, 'v'), (0, 1, '<'), (1, 0, '^'), (0, -1, '>')]
+
+
+q = deque()
+q.extend(Es)
+# print(q)
+
+while q:
+  y, x = q.popleft()
+  for dy, dx, dd in DIRS:
+    ny = dy + y
+    nx = dx + x
+    if not(0 <= ny < H and 0 <= nx < W):
+      continue
+    if visited[ny][nx] or grid[ny][nx] == '#':
+      continue
+    ans[ny][nx] = dd
+    visited[ny][nx] = True
+    q.append((ny, nx))
+
+for row in ans:
+  print(''.join(row))
+
