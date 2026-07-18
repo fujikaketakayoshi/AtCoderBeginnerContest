864a91c89a964aec45a464b28a9eed82144c4975
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-07-18 15:13:03 +0900

    ABのsetを削除

--- 402/d_kaisetsu.py
+++ 402/d_kaisetsu.py
@@ -5,11 +5,9 @@ import math
 N, M = map(int, input().split())
 # print(N, M)
 
-AB = set()
 cnt = [0] * N
 for m in range(M):
   A, B = map(int, input().split())
-  AB.add((A, B))
   mod = (A + B) % N
   cnt[mod] += 1
 # print(cnt)
